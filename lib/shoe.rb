bf8391374fe9d7a45b76bc398a4fd5b68b20f26e
Your Name <you@example.com>
2019-05-15 14:15:52 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,24 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  BRANDS = []
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  # iterate through all the brand and add them to BRANDS
+
+  # iterate through all the brand and add UNIQUE to BRANDS
+  # since we have an array use uniq()?
+  #
+
 
 end
